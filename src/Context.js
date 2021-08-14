539c5a876c2b36d00bc351b6c881ac38372a32f8
Nate Denys <nate_denys@yahoo.com>
2021-08-14 15:29:58 -0700

    Using fetch to obtain images from database
    Updating Context.js to incorporate fetch and console logging url links

--- src/Context.js
+++ src/Context.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Context = React.createContext()
 
 function ContextProvider({children}) {
+    const [allPhotos, setAllPhotos] = useState([])
+
+    const url = "https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json"
+
+    useEffect(() => {
+        fetch(url)
+            .then(response => response.json())
+            .then(data => setAllPhotos(data))
+    }, [])
+
+    console.log(allPhotos)
+
     return (
-        <Context.Provider value="">
+        <Context.Provider value="{{allPhotos}}">
             {children}
         </Context.Provider>
     )
