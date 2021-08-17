b7ac9bdd3bf04806981461eaceadd1280c0cfd78
Nate Denys <nate_denys@yahoo.com>
2021-08-17 13:02:56 -0700

    applying useContext and useEffect hook
    Creating Image component with receiving props and pulling img src from github repo url through fetch call

--- src/Context.js
+++ src/Context.js
@@ -1,3 +1,5 @@
+// Utilizing Context.Provider and the useEffect Hook to pull images from github repo to populate data store
+
 import React, { useState, useEffect } from 'react';
 
 const Context = React.createContext()
@@ -8,15 +10,14 @@ function ContextProvider({children}) {
     const url = "https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json"
 
     useEffect(() => {
+        // url fetch for data and set allPhotos to the data
         fetch(url)
             .then(response => response.json())
             .then(data => setAllPhotos(data))
     }, [])
 
-    console.log(allPhotos)
-
     return (
-        <Context.Provider value="{{allPhotos}}">
+        <Context.Provider value={{allPhotos}}>
             {children}
         </Context.Provider>
     )
--- src/pages/Photos.js
+++ src/pages/Photos.js
@@ -1,9 +1,19 @@
-import React from "react"
+import React, {useContext} from "react"
+
+import Image from "../components/Image"
+import {Context} from "../Context"
+import {getClass} from "../utils"
 
 function Photos() {
+    const {allPhotos} = useContext(Context)
+    console.log(allPhotos)
+
+    const imageElements = allPhotos.map((img, i) => (
+        <Image key={img.id} img={img} className={getClass(i)} />
+        ))
     return (
         <main className="photos">
-            <h1>Images go here</h1>
+            {imageElements}
         </main>
     )
 }
