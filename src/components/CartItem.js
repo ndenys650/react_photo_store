ab8aab9fd0bb5191fbb146381cf902f0f2751269
Nate Denys <nate_denys@yahoo.com>
2021-08-18 11:11:02 -0700

    Creating CartItem component
    Adding simple styling to Cart page and mapping over items from Context object to create list of items on Cart page. Adding remove from Cart functionality on trash can click

---
+++ src/components/CartItem.js
@@ -0,0 +1,16 @@
+import React, {useContext} from 'react'
+import {Context} from '../Context'
+
+function CartItem({item}) {
+    const {removeFromCart} = useContext(Context)
+    return (
+        <div className="cart-item">
+            <i className="ri-delete-bin-line" onClick={() => removeFromCart(item.id)}></i>
+            <img src={item.url} width="130px" />
+            <p>$5.99</p>
+        </div>
+        )   
+
+}
+
+export default CartItem
